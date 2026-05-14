581d113a770cf415b35f84f8620ac3d4ed1d1aef
lk1827 <lucaskauanmiranda86@gmail.com>
2026-05-14 17:35:07 -0300

    Troca dos sprites

--- parit/Assets/Scripts/Gerais/Paineis.cs
+++ parit/Assets/Scripts/Gerais/Paineis.cs
@@ -8,11 +8,15 @@ public class Paineis : MonoBehaviour
     public GameObject texto;
     void Start()
     {
-        GetComponentInChildren<SeguidorDeAlvoRigidbody2D>().enabled = false;  
+        GetComponentInChildren<SeguidorDeAlvoRigidbody2D>().enabled = false;
+        GetComponentInChildren<Fome>().enabled = false;
+        GetComponentInChildren<Spawner>().enabled = false;
     }
     public void Ativar()
     {
         GetComponentInChildren<SeguidorDeAlvoRigidbody2D>().enabled = true;
+        GetComponentInChildren<Fome>().enabled = true;
+        GetComponentInChildren<Spawner>().enabled = true;
         painel.SetActive(false);
         botao.SetActive(false);
         texto.SetActive(false);
